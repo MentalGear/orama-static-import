ca9129c75db987320919bc48ff9bacf54bc9b437
mentalgear <2837147+MentalGear@users.noreply.github.com>
2023-10-18 16:46:00 +0200

    chore: prevent generationg docs sitemap

--- packages/docs/next-sitemap.config.js
+++ packages/docs/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  generateRobotsTxt: true,
-  siteUrl: 'https://docs.oramasearch.com',
-}
+// module.exports = {
+//   generateRobotsTxt: true,
+//   siteUrl: 'https://docs.oramasearch.com',
+// }
